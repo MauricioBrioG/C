cdae94bf7814ef4e3816f606053d38975f7b38c3
MauricioBrioG <78566974+MauricioBrioG@users.noreply.github.com>
2021-04-22 13:40:40 -0500

    Update and rename homework/Homework_4/cons.h to homework/Homework_5/cons.h

--- homework/Homework_5/cons.h
+++ homework/Homework_5/cons.h
@@ -1,9 +1,9 @@
-int dimension= 10;
+int dimension= 5;
 void printboard(char[][dimension]);
 int numberbombs(int,int,int[][2]);
 int movement(char[][dimension], char[][dimension], int[][2], int, int, int*);
 void randombomb(int[][2]);
 void instruccion(int*, int*);
 int valid(int,int);
-int BOMB = 20;
+int BOMB = 6;
 double tol = 0.1;
