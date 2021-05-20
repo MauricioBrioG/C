921b9364f91a83050564e8203aafc6ca1e1d7437
MauricioBrioG <78566974+MauricioBrioG@users.noreply.github.com>
2021-05-20 13:14:54 -0500

    Update header.h

--- homework/PIA/header.h
+++ homework/PIA/header.h
@@ -2,6 +2,6 @@ char aux;
 char nombre[50];
 char adop[50];    
 int opcion = 0;
-void registrar();
-void adoptar();
-void lista();
+void registrar(); //Register the description of the dogs
+void adoptar(); //Add to a list of adoption
+void lista(); //Add to a list of dogs in adoption
